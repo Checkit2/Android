74d82872ec7319110ceafdeceec6de6e38a2213c
amirjabbari <amirjabbari1380@gmail.com>
2021-03-20 15:16:43 +0330

    Check Internet Connection and Network error dialog

--- app/src/main/java/ir/Peaky/checkit/activity/SplashActivity.java
+++ app/src/main/java/ir/Peaky/checkit/activity/SplashActivity.java
@@ -2,15 +2,68 @@ package ir.Peaky.checkit.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import ir.Peaky.checkit.R;
 
 public class SplashActivity extends AppCompatActivity {
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        RelativeLayout relRetry;
+        dialog = new Dialog(SplashActivity.this);
+        dialog.setContentView(R.layout.internet_dialog);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.shape_dialog));
+        }
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+        relRetry = dialog.findViewById(R.id.rel_try);
+        relRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recreate();
+                dialog.dismiss();
+            }
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isNetworkAvailable(getApplicationContext())) {
+                    Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                } else {
+                    dialog.show();
+                }
+            }
+        }, 2500);
+
+
+    }
+
+
+
+
+    public boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected()
+                && connectivityManager.getActiveNetworkInfo().isAvailable();
     }
 }
