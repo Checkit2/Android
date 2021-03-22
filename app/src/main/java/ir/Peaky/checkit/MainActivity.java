73f5a1219446900ba2c1d563968e48638e1ebc7e
amirjabbari <amirjabbari1380@gmail.com>
2021-03-22 16:37:32 +0430

    Bottom sheet Added

--- app/src/main/java/ir/Peaky/checkit/MainActivity.java
+++ app/src/main/java/ir/Peaky/checkit/MainActivity.java
@@ -1,20 +1,47 @@
 package ir.Peaky.checkit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class MainActivity extends AppCompatActivity {
     Window window;
     View view;
+    AppCompatImageView iconImagePicker;
+    private BottomSheetDialog bottomSheetDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         statusbarColor();
+        init();
+
+
+
+        iconImagePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog=new BottomSheetDialog(MainActivity.this,R.style.BottomSheetTheme);
+                View sheetView= LayoutInflater.from(getApplicationContext()).inflate(R.layout.bottom_sheet_layout,
+                        (LinearLayout) findViewById(R.id.bottom_sheet));
+                bottomSheetDialog.setContentView(sheetView);
+
+                bottomSheetDialog.show();
+            }
+        });
+
+    }
+    public void init(){
+        iconImagePicker=findViewById(R.id.icon_camera);
     }
 
     public void statusbarColor(){
