97ca28a5816ffdd7feadbeaa2bf3e7bea231bf3d
amirjabbari <amirjabbari1380@gmail.com>
2021-03-23 22:29:47 +0430

    Add Navigation Drawer

--- app/src/main/java/ir/Peaky/checkit/MainActivity.java
+++ app/src/main/java/ir/Peaky/checkit/MainActivity.java
@@ -1,22 +1,30 @@
 package ir.Peaky.checkit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     Window window;
     View view;
-    AppCompatImageView iconImagePicker;
+    AppCompatImageView iconImagePicker,iconMenu;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
     private BottomSheetDialog bottomSheetDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,24 @@ public class MainActivity extends AppCompatActivity {
         statusbarColor();
         init();
 
+        iconMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id=item.getItemId();
+                switch (id){
+                    case R.id.help:
+                        Toast.makeText(MainActivity.this, "Help", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
 
 
         iconImagePicker.setOnClickListener(new View.OnClickListener() {
@@ -42,7 +68,12 @@ public class MainActivity extends AppCompatActivity {
     }
     public void init(){
         iconImagePicker=findViewById(R.id.icon_camera);
+        navigationView=findViewById(R.id.nav_view);
+        drawerLayout=findViewById(R.id.drawer);
+        iconMenu=findViewById(R.id.icon_menu);
     }
+
+
 
     public void statusbarColor(){
         if (Build.VERSION.SDK_INT>=21){
