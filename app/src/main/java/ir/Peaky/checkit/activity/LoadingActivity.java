af762b642c09b8df144c8aea27641cfe6f4579b6
amirjabbari <amirjabbari1380@gmail.com>
2021-03-26 16:43:56 +0430

    Add LoadingScan Activity and Using Lottie for animation

---
+++ app/src/main/java/ir/Peaky/checkit/activity/LoadingActivity.java
@@ -0,0 +1,16 @@
+package ir.Peaky.checkit.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.Peaky.checkit.R;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
