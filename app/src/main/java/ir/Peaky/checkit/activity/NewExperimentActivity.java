4393691c754340e840c9ca45b0eed21ac44406bc
amirjabbari <amirjabbari1380@gmail.com>
2021-03-26 16:03:22 +0430

    Add Spinner to experiment activity

--- app/src/main/java/ir/Peaky/checkit/activity/NewExperimentActivity.java
+++ app/src/main/java/ir/Peaky/checkit/activity/NewExperimentActivity.java
@@ -1,16 +1,68 @@
 package ir.Peaky.checkit.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import ir.Peaky.checkit.R;
 
 public class NewExperimentActivity extends AppCompatActivity {
+    Window window;
+    View view;
+    AppCompatSpinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_experiment);
+        statusbarColor();
+        init();
+        setSpinner();
+
+    }
+
+
+    public void init() {
+        spinner = findViewById(R.id.spinner);
+
+    }
+
+
+    public void setSpinner() {
+        String value[] = {"آزمایش ادرار","آزمایش مثال"};
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(value));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.spinner_layout_style, arrayList);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item=parent.getItemAtPosition(position).toString();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    public void statusbarColor() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            window = this.getWindow();
+            window.setStatusBarColor(getResources().getColor(R.color.white));
+            view = getWindow().getDecorView();
+            view.setSystemUiVisibility(view.getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
     }
 }
