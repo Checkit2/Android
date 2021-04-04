588d9db3338874fcf537aa7d84354130d359325e
amirjabbari <amirjabbari1380@gmail.com>
2021-04-04 11:27:42 +0430

    fix LoadingActivity ui add cancel dialog for on backPressed

--- app/src/main/java/ir/Peaky/checkit/activity/LoadingActivity.java
+++ app/src/main/java/ir/Peaky/checkit/activity/LoadingActivity.java
@@ -2,11 +2,14 @@ package ir.Peaky.checkit.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.RelativeLayout;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -18,20 +21,26 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+import ir.Peaky.checkit.MainActivity;
 import ir.Peaky.checkit.R;
 import ir.Peaky.checkit.config.PrefManager;
+import ir.Peaky.checkit.utils.BoldTextView;
 import ir.Peaky.checkit.webservice.Constants;
 
 public class LoadingActivity extends AppCompatActivity {
     Window window;
     View view;
     String imageUrl,checkTitle=null;
+    BoldTextView cancelTxt;
     PrefManager prefManager;
+    Dialog dialog;
+    RelativeLayout relYes,relNo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         statusbarColor();
+        init();
         prefManager=new PrefManager(getApplicationContext());
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
@@ -39,8 +48,25 @@ public class LoadingActivity extends AppCompatActivity {
             imageUrl=bundle.getString("imageUrl");
             checkTitle=bundle.getString("checkName");
         }
+        cancelTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+
+
+
+
+
 
     }
+
+    private void init() {
+        cancelTxt=findViewById(R.id.cancel);
+    }
+
+
     public void statusbarColor() {
         if (Build.VERSION.SDK_INT >= 21) {
             window = this.getWindow();
@@ -71,4 +97,38 @@ public class LoadingActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onBackPressed() {
+
+        showDialog();
+    }
+
+    private void showDialog() {
+        dialog = new Dialog(LoadingActivity.this);
+        dialog.setContentView(R.layout.cancel_dialog);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.shape_dialog));
+        }
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        dialog.setCancelable(true);
+        dialog.setCanceledOnTouchOutside(true);
+        relYes=dialog.findViewById(R.id.rel_yes);
+        relNo=dialog.findViewById(R.id.rel_no);
+        relYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent1);
+                finish();
+            }
+        });
+        relNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
 }
